94d01236924775e1e52a129cbd0b6e58e0674292
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-10-01 23:07:47 +0200

    Split issue text

--- .github/parse_issue.py
+++ .github/parse_issue.py
@@ -3,6 +3,9 @@ import pathlib
 
 
 issue_file = sys.argv[1]
-issue_text = pathlib.Path(issue_file).read_text().splitlines(False)
+issue_text = pathlib.Path(issue_file).read_text()
 
 print(issue_text)
+
+sections = issue_text.split("###")
+print(sections)
