ee6a42e6f467496fd3851ba8d830975c3f9dca10
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-10-01 23:13:11 +0200

    Make scenario dict

--- .github/parse_issue.py
+++ .github/parse_issue.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import pprint
 
 
 issue_file = sys.argv[1]
@@ -7,5 +8,12 @@ issue_text = pathlib.Path(issue_file).read_text()
 
 print(issue_text)
 
-sections = issue_text.split("###")
+sections = [s.strip() for s in issue_text.split("###") if s]
 print(sections)
+
+scenario = {}
+for section in sections:
+  key, value = section.strip().split("\n\n")
+  scenario[key] = value
+
+pprint.pprint(scenario)
